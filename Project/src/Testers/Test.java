a618156288a4583a697e3c49ab32c88b39504bd7
jakob garricks <garricksjakob@gmail.com>
2018-11-08 15:50:58 -0800

    made maven directory for library

--- Project/src/Testers/Test.java
+++ Project/src/Testers/Test.java
@@ -1,15 +1,42 @@
 package Testers;
 import Sorters.*;
+import java.util.Scanner;
 public class Test {
 
     public static void main(String args[]){
         try {
-            System.out.println("A".compareTo("b"));
-            CountingSort CountingSort = new CountingSort();
-            CountingSort.load();
-            System.out.println(CountingSort.list);
-            CountingSort.test();
-            System.out.println(CountingSort.list);
+            Scanner reader = new Scanner(System.in);  // Reading from System.in
+            System.out.println("What sort do you want to use: ");
+            System.out.println("1. Counting Sort");
+            System.out.println("2. Quick Sort");
+            System.out.println("3. Merge Sort");
+            int n = reader.nextInt();
+
+            System.out.println("What sort do you want to use: ");
+            System.out.println("1: Numbers");
+            System.out.println("2: Strings");
+            int x = reader.nextInt();
+            if(n == 1) {
+                CountingSort CountingSort = new CountingSort();
+                CountingSort.load();
+                System.out.println(CountingSort.list);
+                CountingSort.test();
+                System.out.println(CountingSort.list);
+            }else if(n==2){
+                QuickSort QuickSort = new QuickSort();
+                QuickSort.load();
+                System.out.println(QuickSort.list);
+                QuickSort.test();
+                System.out.println(QuickSort.list);
+            }
+            else if(n==3)
+            {
+                MergeSort MergeSort = new MergeSort();
+                MergeSort.load();
+                System.out.println(MergeSort.list);
+                MergeSort.test();
+                System.out.println(MergeSort.list);
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
--- Sorting/src/main/java/CountingSort.java
+++ Sorting/src/main/java/CountingSort.java
@@ -1,29 +1,11 @@
-package Sorters;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-
 import java.util.List;
-import java.util.stream.Stream;
 
 public class CountingSort {
 
     public List list = new ArrayList<Integer>();
 
-    public void load(){
-        try (Stream<String> stream = Files.lines(Paths.get("src\\Testers\\numbers.txt"))) {
-            stream.forEach(x -> list.add(Integer.parseInt(x)));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void test(){
-            sort(list);
-    }
-
-    private void sort(List list){
+    public List sort(List list){
 
         int length = list.size();
         List newList = new ArrayList<Integer>();
@@ -43,6 +25,7 @@ public class CountingSort {
                 counter++;
             }
         }
+        return list;
     }
 }
 
